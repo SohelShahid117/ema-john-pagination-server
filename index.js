2e6f6d4721ffcd9e5996077a4bc8ae60ad98f77e
SohelShahid117 <sohelshahid09@gmail.com>
2024-08-28 19:48:00 +0600

    done 62-5 (Interesting) Set current page state and next prev button

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
-require('dotenv').config() 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://emaJhonShop:ci7U2ESoR999OptV@cluster0.hfhifix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-console.log(uri)
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://emaJhonShop:ci7U2ESoR999OptV@cluster0.hfhifix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,25 +27,26 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const productCollection = client.db("emaJohnDB").collection("products");
     // console.log(productCollection)
 
-    app.get('/products', async(req, res) => {
+    app.get("/products", async (req, res) => {
       const result = await productCollection.find().toArray();
       // console.log(result)
       res.send(result);
-    })
+    });
 
-    app.get("/productsCount",async(req,res)=>{
-      const count = await productCollection.estimatedDocumentCount()
-      console.log(count)
-      res.send({count})
-    })
+    app.get("/productsCount", async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      console.log(count);
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,12 +54,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!enJoy MERN')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!enJoy MERN");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`server is running at http://localhost:${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+  console.log(`server is running at http://localhost:${port}`);
+});
+
+//62-5 (Interesting) Set current page state and next prev button
